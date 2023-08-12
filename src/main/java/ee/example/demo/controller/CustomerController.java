dcb23173648703428bb8e8273a52a0ed0c81209f
Hero777 <clickker@mail.ru>
2023-08-12 02:58:37 +0200

    3. Made all controllers

--- src/main/java/ee/example/demo/controller/CustomerController.java
+++ src/main/java/ee/example/demo/controller/CustomerController.java
@@ -1,16 +1,42 @@
 package ee.example.demo.controller;
 
 import ee.example.demo.domain.Customer;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 
-    @PostMapping("/addCustomer")
+    @PostMapping
     public String save(@RequestBody Customer customer) {
         System.out.println("Customer " + customer.getLastName() + " added");
         return "Customer " + customer.getFirstName() + " saved to db.";
+    }
+
+    @GetMapping
+    public List<Customer> getAll() {
+        System.out.println("Get all customers.");
+        return List.of(new Customer(1, "Oleg", "Petrov"));
+    }
+
+    @PutMapping
+    public String changeLastName(@RequestParam int id, @RequestParam String lastName) {
+
+        System.out.println("Change lastname to " + lastName);
+        return "Lastname is change.";
+    }
+
+    @DeleteMapping
+    public String delete(@RequestParam int id) {
+        System.out.println("Deleted customer with id = " + id);
+        return "Successfully deleted.";
+    }
+
+    @GetMapping("/{id}")
+    public Customer getById(@PathVariable int id) {
+        System.out.println("Get customer with id = " + id);
+        return new Customer(1,"Oleg", "Sidorov");
     }
 }
